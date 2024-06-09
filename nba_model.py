d8b9465f6563a38564e18a3d5d1a9b1294d6c9a4
Anuj Bhandari <76541094+anujbhandari2@users.noreply.github.com>
2024-06-08 20:58:44 -0700

    Update nba_model.py

--- nba_model.py
+++ nba_model.py
@@ -1,9 +1,9 @@
 '''
-NFL MODEL
+NBA MODEL
 
 Standard Performance: Low to Mid 70s %
 Desired Performance: 65%
-Achieved Performance: 61%
+Achieved Performance: 62%
 
 Results from running Code:
 
@@ -16,6 +16,8 @@ Random Forest Results:
 Accuracy: 0.6123309763056692
 Mean Squared Error: 0.3876690236943307
 Mean Absolute Error: 0.3876690236943307
+Accuracy with Tuning: 0.6205185460860936
+Change: +0.01
 
 Logistic Regression Results:
 Accuracy: 0.6076169209775462
@@ -26,13 +28,15 @@ Decision Tree Results:
 Accuracy: 0.5772236695199107
 Mean Squared Error: 0.42277633048008934
 Mean Absolute Error: 0.42277633048008934
+Accuracy with Tuning: 0.6136955712690734
+Change: +0.04
 
 Neural Network Results:
 Accuracy: 0.6076169209775462
 Mean Squared Error: 0.3923830790224538
 Mean Absolute Error: 0.3923830790224538
 
-The best model is: Random Forest with an accuracy of 0.6123309763056692
+The best model is: Random Forest with an accuracy of 0.6205185460860936
 
 '''
 
@@ -78,9 +82,9 @@ x_Train, x_Test, y_Train, y_Test = train_test_split(x, y, test_size=0.2, shuffle
 # models to be tested
 models = {
     'Linear Regression': LinearRegression(),
-    'Random Forest': RandomForestClassifier(random_state=92001),
+    'Random Forest': RandomForestClassifier(random_state=92001, max_depth = 9, min_samples_split = 2, min_samples_leaf = 9),
     'Logistic Regression': LogisticRegression(max_iter=4000),
-    'Decision Tree': DecisionTreeClassifier(random_state=92001),
+    'Decision Tree': DecisionTreeClassifier(random_state=92001, max_depth = 6, min_samples_split = 2, min_samples_leaf = 2),
     'Neural Network': MLPClassifier(hidden_layer_sizes=(100,), max_iter=4000, random_state=92003)
 
 }
@@ -117,3 +121,26 @@ for model_name, model in models.items():
         best_model = model
 
 print(f'The best model is: {best_model_name} with an accuracy of {best_accuracy}')
+
+def predict_winner_nba(home_team, away_team, data, label_encoders):
+    home_team_encoded = label_encoders['TEAM_NAME_HOME'].transform([home_team])[0]
+    away_team_encoded = label_encoders['TEAM_NAME_AWAY'].transform([away_team])[0]
+    
+    home_team_data = data[data['TEAM_NAME_HOMEEncoded'] == home_team_encoded].iloc[0]
+    away_team_data = data[data['TEAM_NAME_AWAYEncoded'] == away_team_encoded].iloc[0]
+    
+    input_data = np.array([
+        home_team_data['GAME_ID'], home_team_data['MATCHUPEncoded'], home_team_encoded, home_team_data['TEAM_ID_HOME'],
+        away_team_encoded, away_team_data['TEAM_ID_AWAY']
+    ]).reshape(1, -1)
+    
+    probabilities = best_model.predict_proba(input_data)[0]
+    
+    result = {
+        'home_win_prob': probabilities[0],
+        'draw_prob': 0,
+        'away_win_prob': probabilities[1]
+    }
+    
+    return result
+
